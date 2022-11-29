2a7bfe5bb4a2a7b3cf98914b247a6a0a56f7b19a
Halszka Ciesielska <halszka99@interia.eu>
2022-11-29 17:29:21 +0100

    buffer cache flash

--- DBAnalysisApp/Program.cs
+++ DBAnalysisApp/Program.cs
@@ -1,10 +1,10 @@
 ﻿using DBAnalysisApp;
 
-string connString = "DATA SOURCE=localhost:1521/XEPDB1;PERSIST SECURITY INFO=True;USER ID=ZSBD_ADM; password=passwd; Pooling = False; ";
+string connString = "DATA SOURCE=localhost:1521/XEPDB1;PERSIST SECURITY INFO=True;USER ID=ZSBD_ADM; password=passwd; Pooling = False; Statement Cache Size=0";
 Console.WriteLine("Number of tests:");
 int numberOfTests = Convert.ToInt32(Console.ReadLine());
 Dictionary<string, List<double>> times = new Dictionary<string, List<double>>();
-string[] files = new[] { "select1", "select2", "select3", "insert", "update", "delete" };
+string[] files = new[] { "select1", "select2", "select3"};
 foreach (string file in files)
 {
   times.Add(file,new List<double>());
@@ -16,18 +16,19 @@ for (int i=0; i< numberOfTests; i++)
   string previous = null;
   foreach (string file in files)
   {
-    times[file].Add(QuerryRunner.RunAllForQuerry($"D:\\GitHub\\DBAnalysisApp\\DBAnalysisApp\\Query\\{file}.sql", connString, previous?.Contains("select") ?? true));
+    times[file].Add(QuerryRunner.RunAllForQuerry($"..\\..\\..\\Query\\{file}.sql", connString, previous?.Contains("select") ?? true));
     previous = file;
   }
  
 }
 
-using StreamWriter outputfile = new($"times.txt");
+using StreamWriter outputfile = new($"..\\..\\..\\Plans\\times.txt");
 foreach (string file in files)
 {
+    var x = times[file];
   outputfile.WriteLine($"{file}:");
   outputfile.WriteLine($"Number of runs: {numberOfTests}");
   outputfile.WriteLine($"Min time: {times[file].Min()} ms");
   outputfile.WriteLine($"Max time: {times[file].Max()} ms");
-  outputfile.WriteLine($"Max time: {times[file].Average()} ms");
+  outputfile.WriteLine($"Avg time: {times[file].Average()} ms");
 }
--- DBAnalysisApp/QuerryRunner.cs
+++ DBAnalysisApp/QuerryRunner.cs
@@ -1,7 +1,9 @@
 ﻿using Oracle.ManagedDataAccess.Client;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Drawing;
 using System.Linq;
 using System.Reflection.PortableExecutable;
 using System.Text;
@@ -11,55 +13,81 @@ namespace DBAnalysisApp
 {
   public static class QuerryRunner
   {
-    private static void ClearBuffers(OracleConnection connection)
+    private static void ClearBuffers(string connectionString)
     {
-        var command = new OracleCommand("alter system flush buffer_cache", connection);
-        command.ExecuteNonQuery();
+        using (OracleConnection connection = new OracleConnection(connectionString))
+        {
+            connection.Open();
+            var command = new OracleCommand("alter system flush buffer_cache", connection);
+            command.ExecuteReader();
+            connection.Close();
+            connection.Dispose();
+        }
     }
 
-    private static double RunQuery(string query, OracleConnection connection)
+    private static double RunQuery(string query, string connectionString)
     {
       DateTime start;
       DateTime end;
       double sum = 0;
-      foreach (var subQuery in query.Split(';').Where(x => !String.IsNullOrEmpty(x)))
-      {
-        start = DateTime.Now;
-        var command = new OracleCommand(subQuery, connection);
-        command.ExecuteNonQuery();
-        end = DateTime.Now;
-        sum += (end - start).TotalMilliseconds;
-      }
-      return sum;
+
+        using (OracleConnection connection = new OracleConnection(connectionString))
+        {
+            connection.Open();
+            foreach (var subQuery in query.Split(';').Where(x => !String.IsNullOrEmpty(x)))
+            {
+                start = DateTime.Now;
+                var command = new OracleCommand(subQuery, connection);
+                var reader = command.ExecuteReader();
+
+                end = DateTime.Now;
+                sum += (end - start).TotalMilliseconds;
+            }
+            connection.Close();
+            connection.Dispose();
+            return sum;
+        }
+
     }
 
-    private static void GetPlans(string query, string fileName, OracleConnection connection)
+    private static void GetPlans(string query, string fileName, string connectionString)
     {
 
-      using StreamWriter file = new($"plan_{fileName}.txt");
-      foreach (var subQuery in query.Split(';').Where(x=> !String.IsNullOrEmpty(x)))
-      {
-        var command = new OracleCommand($"EXPLAIN PLAN FOR {subQuery}", connection);
-        command.ExecuteNonQuery();
-        var command2 = new OracleCommand($"select plan_table_output from table(dbms_xplan.display('plan_table',null,'basic +predicate +cost'))", connection);
-        var reader = command2.ExecuteReader();
-        try
+        using StreamWriter file = new($"..\\..\\..\\Plans\\plan_{fileName}.txt");
+        using (OracleConnection connection = new OracleConnection(connectionString))
         {
-          while (reader.Read())
-          {
-            file.WriteLine(reader.GetString(0));
-          }
+            connection.Open();
+            foreach (var subQuery in query.Split(';').Where(x => !String.IsNullOrEmpty(x)))
+            {
+                var command = new OracleCommand($"EXPLAIN PLAN FOR {subQuery}", connection);
+                command.ExecuteNonQuery();
+                var command2 = new OracleCommand($"select plan_table_output from table(dbms_xplan.display('plan_table',null,'basic +predicate +cost'))", connection);
+                var reader = command2.ExecuteReader();
+                try
+                {
+                    while (reader.Read())
+                    {
+                        file.WriteLine(reader.GetString(0));
+                    }
+                }
+                finally
+                {
+                    reader.Close();
+                }
+            }
+            connection.Close();
+            connection.Dispose();
         }
-        finally
-        {
-          reader.Close();
-        }
-      }
     }
 
     private static void BackupDatabase(string connectionString)
     {
-
+        using (OracleConnection connection = new OracleConnection(connectionString))
+        {
+            connection.Open();
+            connection.Close();
+            connection.Dispose();
+        }
     }
 
     public static double RunAllForQuerry(string fileName, string connectionString, bool lastOperationNotChangedDb)
@@ -68,16 +96,13 @@ namespace DBAnalysisApp
       if(!lastOperationNotChangedDb)
         BackupDatabase(connectionString);
 
-      using (OracleConnection connection = new OracleConnection(connectionString))
-      {
-        connection.Open();
-        //ClearBuffers(connection);
-        GetPlans(query, fileName.Split('\\').Last().Split('.').First(), connection);
-        //ClearBuffers(connection);
-        var time = RunQuery(query, connection);
-        connection.Close();
+
+        GetPlans(query, fileName.Split('\\').Last().Split('.').First(), connectionString);
+        ClearBuffers(connectionString);
+
+        var time = RunQuery(query, connectionString);
+        ClearBuffers(connectionString);
         return time;
-      }
     }
   }
 }
